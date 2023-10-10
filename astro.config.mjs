9d0b93c2860c79ef5747b623e35bb0bd430a06cb
Jerad Maplethorpe <jeradmaplethorpe@gmail.com>
2023-10-10 09:59:06 -0500

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config"
-import node from "@astrojs/node"
 import react from "@astrojs/react"
-
 import tailwind from "@astrojs/tailwind"
+
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +15,5 @@ export default defineConfig({
     }),
   ],
   output: "server",
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
 })
